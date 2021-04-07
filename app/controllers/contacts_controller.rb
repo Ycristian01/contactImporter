880a4508dc0a7593e3d082f5961a15fe0191e077
Cristian Yepes <ycristian@uninorte.edu.co>
2021-04-07 18:02:40 -0500

    added contact_controller methods

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,56 @@
 class ContactsController < ApplicationController
+  before_action :set_user
+  before_action :set_contact, only: %i[ show edit update destroy ]
+
+  def show
+  end
+
+  def new
+   @contact = @user.contacts.new 
+  end
+
+  def edit
+  end
+
+  def create
+    @contact = @user.contacts.create(contact_params)
+ 
+     if @contact.save
+       redirect_to user_contact_path @user , @contact
+       flash[:notice] = "Contact was successfully created"
+     else
+       render 'new'
+     end
+   end
+
+  def update
+    if @contact.update(contact_params) 
+      redirect_to user_contact_path @user
+      flash[:notice] = "contact was successfully updated." 
+    else
+      render 'edit'
+    end   
+  end
+
+  def destroy
+    @contact.destroy
+
+    redirect_to user_path @user
+    flash[:notice] = "contact was successfully destroyed." 
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+      
+    def set_contact
+      @contact = @user.contacts.find(params[:id]) 
+    end
+
+    def contact_params
+      params.require(:contact).permit(:name, :dayOfBirth, :phone, :address, :card, :franchise, :email) 
+    end
+
 end
